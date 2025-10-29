7459af3bed5cf1683680b5185bc78f727b9b0204
AXASHISH <ashishkumarsahoo.8021@gmail.com>
2025-10-29 13:15:56 +0530

    Navbar component is fixed at the top of the page while scrolling

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -14,59 +14,59 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            {/* Left: Logo */}
-            <div className="nav-logo">
-                <img src={logo} alt="logo" />
-                <p>Craftshop</p>
-            </div>
-
-
-            <div className={`nav-menu ${isOpen ? 'open' : ''}`}>
-                {/* Nav Links */}
-                <ul className="nav-links">
-                    <li onClick={() => { setMenu("Home"); setIsOpen(false); }}>
-                        <Link to="/">Home</Link>{menu === "Home" ? <hr /> : null}
-                    </li>
-                    <li onClick={() => { setMenu("Products"); setIsOpen(false); }}>
-                        <Link to="/products">Products</Link>{menu === "Products" ? <hr /> : null}
-                    </li>
-                    <li onClick={() => { setMenu("About"); setIsOpen(false); }}>
-                        <Link to="/about">About</Link>{menu === "About" ? <hr /> : null}
-                    </li>
-                    <li onClick={() => { setMenu("Contact"); setIsOpen(false); }}>
-                        <Link to="/contact">Contact</Link>{menu === "Contact" ? <hr /> : null}
-                    </li>
-                </ul>
-
-                {/* Right: Login & Cart */}
-                <div className="right-nav">
-                <div className={`nav-login ${isOpen ? 'open' : ''}`}>
-                    <Link to="/login">
-                        <button onClick={() => { setIsOpen(false); }}>Login</button>
-                    </Link>
-                    <div className="cart">
-                        <Link to="/cart">
-                            <img src={cart_icon} alt="cart" />
-                        </Link>
-                        <div className="cart-count">0</div>
-                    </div>
+            <div className="navbarin">
+                {/* Left: Logo */}
+                <div className="nav-logo">
+                    <img src={logo} alt="logo" />
+                    <p>Craftshop</p>
                 </div>
 
-                <div className='profile_menu'>
-                    <p>Ashish Kumar Sahoo</p>
-                    <  div className="profile-icon">
-                <Link to="#">
-                        <User size={30} color="#fff" />
-                </Link>
+                <div className={`nav-menu ${isOpen ? 'open' : ''}`}>
+                    {/* Nav Links */}
+                    <ul className="nav-links">
+                        <li onClick={() => { setMenu("Home"); setIsOpen(false); }}>
+                            <Link to="/">Home</Link>{menu === "Home" ? <hr /> : null}
+                        </li>
+                        <li onClick={() => { setMenu("Products"); setIsOpen(false); }}>
+                            <Link to="/products">Products</Link>{menu === "Products" ? <hr /> : null}
+                        </li>
+                        <li onClick={() => { setMenu("About"); setIsOpen(false); }}>
+                            <Link to="/about">About</Link>{menu === "About" ? <hr /> : null}
+                        </li>
+                        <li onClick={() => { setMenu("Contact"); setIsOpen(false); }}>
+                            <Link to="/contact">Contact</Link>{menu === "Contact" ? <hr /> : null}
+                        </li>
+                    </ul>
+
+                    {/* Right: Login & Cart */}
+                    <div className="right-nav">
+                        <div className={`nav-login ${isOpen ? 'open' : ''}`}>
+                            <div className="cart">
+                                <Link to="/cart">
+                                    <img src={cart_icon} alt="cart" />
+                                </Link>
+                                <div className="cart-count">0</div>
+                            </div>
+                            <Link to="/login">
+                                <button onClick={() => { setIsOpen(false); }}>Login</button>
+                            </Link>
+
+                        </div>
+
+                        <div className='profile_menu'>
+                            <p>Ashish Kumar Sahoo</p>
+                            <  div className="profile-icon">
+                                <Link to="#">
+                                    <User size={30} color="#fff" />
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                
-
-            </div>
-            </div>
-            {/* Hamburger icon for mobile */}
-            <div className="hamburger" onClick={toggleMenu}>
-                {isOpen ? <X size={28} color="#fff" /> : <Menu size={28} color="#fff" />}
+                {/* Hamburger icon for mobile */}
+                <div className="hamburger" onClick={toggleMenu}>
+                    {isOpen ? <X size={28} color="#fff" /> : <Menu size={28} color="#fff" />}
+                </div>
             </div>
         </div>
 
